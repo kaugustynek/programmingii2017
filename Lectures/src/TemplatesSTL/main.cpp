8e983c8dfdcfd333c8ae24be15488a3438ee6691
Krzysztof Augustynek <kaugustynek@gmail.com>
2017-05-27 18:35:03 +0200

    vectors and lists samples

--- Lectures/src/TemplatesSTL/main.cpp
+++ Lectures/src/TemplatesSTL/main.cpp
@@ -3,10 +3,24 @@
 
 #include "stdafx.h"
 #include <vector>
+#include <list>
 
-int main()
+void ShowVector(vector<int> v)
 {
-    // 1. Vector
+    for (vector<int>::iterator it = v.begin(); it != v.end(); ++it)
+        cout << ' ' << *it;
+    cout << endl;
+}
+
+void ShowList(list<int> v)
+{
+    for (list<int>::iterator it = v.begin(); it != v.end(); ++it)
+        cout << ' ' << *it;
+    cout << endl;
+}
+
+void VectorsDemo()
+{
     //  Vectors are sequence containers representing arrays that can change in size.
     // constructors used in the same order as described above:
     vector<int> first;                                // empty vector of ints
@@ -15,16 +29,101 @@ int main()
     vector<int> fourth(third);                        // a copy of third
 
                                                       // the iterator constructor can also be used to construct from arrays:
-    double tab[] = { 16,2,77,29 };
-    int size = sizeof(tab) / sizeof(double);
+    int tab[] = { 16,2,77,29 };
+    int size = sizeof(tab) / sizeof(int);
     vector<int> fifth(tab, tab + size);
 
     cout << "The contents of fifth are:";
-    for (vector<int>::iterator it = fifth.begin(); it != fifth.end(); ++it)
+    ShowVector(fifth);
+
+    // it is possible to iterate over reverse array
+
+    cout << "The contents of inverse fifth are:";
+    for (auto it = fifth.rbegin(); it != fifth.rend(); ++it)
         cout << ' ' << *it;
     cout << '\n';
 
+    fifth.push_back(100);
+    cout << "Push 100 after last element: " << endl;
+    ShowVector(fifth);
+
+    fifth.pop_back();
+    cout << "Remove last element: " << endl;
+    ShowVector(fifth);
+
+    cout << "Show the current size of the vector: ";
+    cout << fifth.size() << endl;
+
+    fifth.resize(10);
+
+    cout << "After resizing: ";
+    cout << fifth.size() << endl;
+
+    // It throws exception
+    fifth[10] = 20;
+
+    // more information 
+    //http://www.cplusplus.com/reference/vector/vector/
+}
+
+void ListsDemo()
+{
+    //Lists are sequence containers that allow constant time insert and erase operations anywhere within the sequence, and iteration in both directions.
+    //List containers are implemented as doubly - linked lists; Doubly linked lists can store each of the elements they contain in different and unrelated storage locations.The ordering is kept internally by the association to each element of a link to the element preceding it and a link to the element following it.
+
+    // Oznacza to, ¿e elementy listy nie s¹ u³o¿one kolejno w komórkach pamiêci
+    // Konstruktory s¹ podobne, jak w przypadku wektora.
+    list<int> mylist(2, 100);         // two ints with a value of 100
+
+                                      // istnieje mo¿liwoœæ dodawania (usuwania) elementów na pocz¹tek listy
+    mylist.push_front(200);
+    mylist.push_front(300);
+
+    cout << "mylist contains:";
+    ShowList(mylist);
+
+    mylist.resize(5, 20);
+    cout << "after resizing:";
+    ShowList(mylist);
+
+    // operacje
+    mylist.sort();
+    cout << "after sorting:";
+    ShowList(mylist);
+
+    mylist.reverse();
+    cout << "after reversing:";
+    ShowList(mylist);
+
+    mylist.push_back(20);
+
+    mylist.unique();
+    cout << "unique list of elements:";
+    ShowList(mylist);
+
+    int tab[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+    int size = sizeof(tab) / sizeof(int);
+    mylist = list<int>(tab, tab + size);
+
+    cout << "new list:";
+    ShowList(mylist);
+
+    // usuniêcie elementów parzystych
+    mylist.remove_if([](const int& value) { return value % 2 == 0; });
+    cout << "after removing:";
+    ShowList(mylist);
+
+    // other examples see http://www.cplusplus.com/reference/list/list/?kw=list
+}
+
+int main()
+{
+    // 1. Vector
+    //VectorsDemo();
+    
     // 2. List
+    ListsDemo();
+
     // 3. Map
     // 4. Multimap
     // 5. Deque
